9a336c7c88e8b9f230cc87290439c5b827299ead
catomat0 <catomat0@github.com>
2026-07-09 00:04:14 +0900

    chore: 유저 닉네임 빈문자열 미허용

--- src/main/java/com/gominitta/backend/domain/user/dto/request/UserUpdateRequestDTO.java
+++ src/main/java/com/gominitta/backend/domain/user/dto/request/UserUpdateRequestDTO.java
@@ -11,8 +11,8 @@ import lombok.Builder;
 @Schema(description = "유저 프로필 수정 요청")
 public record UserUpdateRequestDTO(
 	@Schema(description = "변경할 닉네임 (미입력 시 변경 없음)", example = "고민이")
-	@Size(max = 10, message = "닉네임은 10자 이하여야 합니다.")
-	@Pattern(regexp = "^[가-힣a-zA-Z0-9]*$", message = "닉네임에 특수문자를 사용할 수 없습니다.")
+	@Size(min = 1, max = 10, message = "닉네임은 1자 이상 10자 이하여야 합니다.")
+	@Pattern(regexp = "^[가-힣a-zA-Z0-9]+$", message = "닉네임에 특수문자를 사용할 수 없습니다.")
 	String nickname,
 
 	@Schema(description = "변경할 프로필 아이콘 (미입력 시 변경 없음)", example = "CHARACTER_1")
