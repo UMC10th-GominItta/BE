e8672ed23aa050ef2878512409d4cd84fcc1c6fb
anhyunjung <anhyunjung0531@gmail.com>
2026-07-25 05:44:33 +0900

    feat: 알림 예외 코드 정의

--- src/main/java/com/gominitta/backend/domain/notification/exception/NotificationErrorCode.java
+++ src/main/java/com/gominitta/backend/domain/notification/exception/NotificationErrorCode.java
@@ -1,4 +1,21 @@
 package com.gominitta.backend.domain.notification.exception;
 
-public enum NotificationErrorCode {
+import org.springframework.http.HttpStatus;
+
+import com.gominitta.backend.global.common.exception.BaseErrorCode;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum NotificationErrorCode implements BaseErrorCode {
+
+	NOTIFICATION_FORBIDDEN(HttpStatus.FORBIDDEN, "NOTIFICATION_40301", "본인의 알림만 조회할 수 있습니다."),
+
+	NOTIFICATION_NOT_FOUND(HttpStatus.NOT_FOUND, "NOTIFICATION_40401", "알림을 찾을 수 없습니다.");
+
+	private final HttpStatus status;
+	private final String code;
+	private final String message;
 }
